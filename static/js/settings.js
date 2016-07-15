12b55f71d405e05d8cb107535b57a797bb2975df
Chenglong Wei <weichenglong@gmail.com>
2016-07-15 15:07:59 -0700

    http request send request based on settings

--- static/js/settings.js
+++ static/js/settings.js
@@ -7,3 +7,18 @@ function ts_debug_off() {
     var ts_url = document.getElementById('ts_address').value;
     document.getElementById("debugOptionFrame").src = ts_url + "?debug=off";
 }
+
+function ts_http_request() {
+    var request_url = document.getElementById('ts_request_url').value;
+
+    var hook_ids = $('#hook_id').val();
+    if (hook_ids != null && hook_ids.indexOf("ALL_HOOKs") > -1) {
+        hook_ids = "ALL_HOOKs";
+    }
+
+    if (hook_ids != null) {
+        request_url += "?hooks=" + hook_ids;
+    }
+
+    document.getElementById("debugOptionFrame").src = request_url;
+}
